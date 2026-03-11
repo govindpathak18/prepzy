ad2f665ec8158ee18535693d333ee989946b04e8
govindpathak18 <govindpathak655@gmail.com>
2026-03-11 09:10:27 +0530

    initial commit

---
+++ backend/src/lib/env.js
@@ -0,0 +1,9 @@
+import dotenv from 'dotenv';
+
+dotenv.config({quiet: true});
+
+export const ENV = {
+    PORT: process.env.PORT || 3000,
+    DB_URL: process.env.DB_URL,
+    
+}
---
+++ backend/src/server.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import  { ENV } from './lib/env.js';
+
+
+const app = express();
+
+
+// make our app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
+  } catch (error) {
+    console.error("💥 Error starting the server", error);
+  }
+};
+
+startServer();
