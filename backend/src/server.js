d035f04988d97e35f6a1058b89ffa7f3a692253e
govindpathak18 <govindpathak655@gmail.com>
2026-03-12 12:19:08 +0530

    uth-db sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,17 @@ import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import { serve } from "inngest/express";
 
 const app = express();
 const __dirname = path.resolve();
 
 app.use(express.json());
+
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 app.get("/api/test", (req, res) => {
   res.json({ message: "API working" });
