4b92f15693261e125c56b3701d4c209687feffc4
govindpathak18 <govindpathak655@gmail.com>
2026-03-12 12:01:39 +0530

    auth and dp sync

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,11 +2,19 @@ import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
+import {serve} from "inngest/express" // for inngest server;
 
 const app = express();
 const __dirname = path.resolve();
 
+// Middleware
 app.use(express.json());
+app.use(cors({origin:ENV.CLIENT_URL,extended:true}));
+// extended:true => server allows a browser to include cookies on reuquest
+
+app.use('/api/inngest',serve({client: inngest, functions}))
+
 
 app.get("/api/test", (req, res) => {
   res.json({ message: "API working" });
