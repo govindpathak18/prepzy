04a8c8de0b1c6760a02b5315c0e0fc10dc2d5f55
govindpathak18 <govindpathak655@gmail.com>
2026-03-20 20:41:28 +0530

    ..

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,46 +1,47 @@
 import express from "express";
 import path from "path";
-import { ENV } from "./lib/env.js";
-import { connectDB } from "./lib/db.js";
 import cors from "cors";
 import { serve } from "inngest/express";
-import { inngest, functions } from "./lib/inngest.js";
 import { clerkMiddleware } from "@clerk/express";
+
+import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
+import { inngest, functions } from "./lib/inngest.js";
+
 import chatRoutes from "./routes/chatRoutes.js";
-import sessionRoutes from "./routes/sessionRoutes.js";
+import sessionRoutes from "./routes/sessionRoute.js";
 
 const app = express();
+
 const __dirname = path.resolve();
 
-// app.use("/api/test", (req, res) => {
-//   res.json({ message: "api testing " });
-// });
-
-app.use(clerkMiddleware()); // this adds auth feild to reques object,: req.auth()
+// middleware
 app.use(express.json());
-
 // credentials:true meaning?? => server allows a browser to include cookies on request
 app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+app.use(clerkMiddleware()); // this adds auth field to request object: req.auth()
 
 app.use("/api/inngest", serve({ client: inngest, functions }));
 app.use("/api/chat", chatRoutes);
 app.use("/api/sessions", sessionRoutes);
 
-// serve frontend
+app.get("/health", (req, res) => {
+  res.status(200).json({ msg: "api is up and running" });
+});
+
+// make our app ready for deployment
 if (ENV.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
 const startServer = async () => {
   try {
     await connectDB();
-    app.listen(ENV.PORT, () =>
-      console.log("🎉 Server is running on port:", ENV.PORT)
-    );
+    app.listen(ENV.PORT, () => console.log("Server is running on port:", ENV.PORT));
   } catch (error) {
     console.error("💥 Error starting the server", error);
   }
