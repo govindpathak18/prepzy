6dbe0c04518d4c4d385142d7537c92688a660acc
govindpathak18 <govindpathak655@gmail.com>
2026-06-22 23:13:43 +0530

    fix: update route handling for serving frontend files

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,7 +35,7 @@ if (ENV.NODE_ENV === "production") {
 
   app.use(express.static(frontendPath));
 
-  app.get("*", (req, res) => {
+  app.get(/.*/, (req, res) => {
     res.sendFile(path.join(frontendPath, "index.html"));
   });
 }
